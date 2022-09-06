244b919df4c01792efa9830daa2ce2e76eab05d6
Megha <meghajagadale99@gmail.com>
2022-09-06 23:43:26 +0530

    Created AddressBook App Project

---
+++ src/main/java/com/bridgelabz/AddressBookApp/AddressBookAppApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.AddressBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Addressbook App");
+		SpringApplication.run(AddressBookAppApplication.class, args);
+	}
+
+
+}
